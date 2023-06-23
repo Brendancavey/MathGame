363e372c452f966f0f1d7d466ff667e247417807
Brendan <bthoeung@gmail.com>
2023-06-22 17:05:39 -0700

    <feat>Add database migrations and connection to SQL server

--- MathGame/Controllers/MathQuestionController.cs
+++ MathGame/Controllers/MathQuestionController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MathGame.Models;
+using System.Security.Cryptography.X509Certificates;
 
 namespace MathGame.Controllers
 {
@@ -8,7 +9,26 @@ namespace MathGame.Controllers
         public IActionResult Random()
         {
             var math_question = new MathQuestion();
-            return View(math_question);
+            var user = new User();
+
+            var viewModel = new QuestionModel()
+            {
+                question = math_question,
+                user = user
+            };
+            
+            return View(viewModel);
         }
+
+        [HttpPost]
+
+        public ActionResult Random(QuestionModel question)
+        {
+            question.user = new User();
+            question.question = new MathQuestion();
+            question.user.score = 1;
+            return View(question);
+        }
+
     }
 }
--- MathGame/Models/MathQuestion.cs
+++ MathGame/Models/MathQuestion.cs
@@ -1,21 +1,30 @@
-﻿namespace MathGame.Models
+﻿using Microsoft.CodeAnalysis;
+
+namespace MathGame.Models
 {
+
     public class MathQuestion
     {
         public int Id { get; set; }
         public int num1 { get; set; }
         public int num2 { get; set; }
+        public int score { get; set; }
+        public string answer { get; set; }
+        public string user_answer { get; set; }
+
         Random rand = new Random();
 
         public MathQuestion()
         {
             this.num1 = rand.Next(1, 11);
             this.num2 = rand.Next(1, 11);
+            this.answer = (this.num1 + this.num2).ToString();
         }
 
-        public string GetQuestion()
+        public string getQuestion()
         {
             return this.num1 + " + " +  this.num2;
         }
+    
     }
 }
